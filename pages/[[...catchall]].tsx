a4de246e63fd16fa2bf4688a7212057d53127c13
Nate Dunn <hello@natedunn.net>
2021-11-10 16:39:09 -0600

    Update fallback

--- pages/[[...catchall]].tsx
+++ pages/[[...catchall]].tsx
@@ -52,7 +52,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
         catchall: mod.path.substring(1).split("/"),
       },
     })),
-    fallback: "block"
+    fallback: "blocking"
   };
 }
 
