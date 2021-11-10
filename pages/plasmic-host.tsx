6760694f9154db675db8c9d03a249f004c98a6d4
Nate Dunn <hello@natedunn.net>
2021-11-10 16:28:43 -0600

    Fix attempt

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react';
 import Script from 'next/script';
 import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
-import Head from 'next/head';
+// import Head from 'next/head';
 import { PLASMIC } from '../plasmic-init';
 
 export default function Host() {
   return PLASMIC && (
     <div>
-      <Head>
-        {/* Nothing */}
-      </Head>
       <Script src="https://static1.plasmic.app/preamble.js" ></Script>
       <PlasmicCanvasHost />
     </div>
